00e8c1f1e6e503f0db2b59f7e0c69ab2109df36c
Joel de Araujo Pereira Junior <joel.junior@estudante.ifro.edu.br>
2026-05-19 21:54:06 -0400

    ajustes

---
+++ Data/AppDbContext.cs
@@ -0,0 +1,25 @@
+﻿using Microsoft.EntityFrameworkCore;
+using WebApiCafeteria.Models;
+
+namespace WebApiCafeteria.Data
+{
+    public class AppDbContext : DbContext
+    {
+        public AppDbContext(DbContextOptions<AppDbContext> options)
+       : base(options)
+        {
+        }
+
+        public DbSet<Livro> Livros { get; set; }
+
+        public DbSet<Evento> Eventos { get; set; }
+
+        public DbSet<Cliente> Clientes { get; set; }
+
+        public DbSet<Funcionario> Funcionarios { get; set; }
+
+        public DbSet<ReservaLivro> ReservasLivros { get; set; }
+
+        public DbSet<ParticipacaoEvento> ParticipacoesEventos { get; set; }
+    }
+}
---
+++ Models/Livro.cs
@@ -0,0 +1,17 @@
+﻿namespace WebApiCafeteria.Models
+{
+    public class Livro
+    {
+        public int Id { get; set; }
+
+        public string Titulo { get; set; }
+
+        public string Autor { get; set; }
+
+        public int Ano { get; set; }
+
+        public int QuantidadeEstoque { get; set; }
+
+        public string? ImagemCapa { get; set; }
+    }
+}
